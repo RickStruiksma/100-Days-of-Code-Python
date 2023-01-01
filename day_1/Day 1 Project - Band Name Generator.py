6698819a2ac54881dd1720b18159bed1714bfa57
Rick Struiksma <rick.struiksma@gmail.com>
2023-01-01 12:01:17 +0100

    Added day 1 exercises

---
+++ day_1/Day 1 Project - Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the python bandname generator!\
+Based on your input, this script will create a super rad band name.")
+city_name = input("Where did you grow up? ")
+pet_name = input("Please enter the name of a childhood pet: ")
+print(f"Your totally rad band name is {city_name} {pet_name}!")
